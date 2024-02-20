e354e83a710dbb865197710325915f70e8b15d66
Michel-2004 <mvilbou1403@g.educaand.es>
2024-02-20 17:14:48 +0100

    crear ventas

--- repuestosMichel.py
+++ repuestosMichel.py
@@ -1,40 +1,70 @@
-from flask import Flask, render_template , request, redirect, url_for, flash
-from getpass import getpass
-from mysql.connector import connect, Error
+from flask import Flask, render_template, request, redirect, url_for, flash
 import mysql.connector
-
-
 
 app = Flask(__name__)
 
-try:
-    with connect(
-        host='127.0.0.1', 
-        port=3306,
-        user= input('usuario: '),
-        password=getpass('la contraseña es: '),
-        database='API',   
-     ) as connection:
-        print(connection)
-except Error as e:
-    print (e)
+mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="contraseña123",
+    database="api"
+)
 
 @app.route("/")
 def Index():
-    base = mysql.connector.connect(database="api")
-    cur = base.connect.cursor()
+    cur = mydb.cursor()
     cur.execute('SELECT * FROM datos_empleados')
     datos = cur.fetchall()
-    return render_template('index.html', contactos = datos)
+    cur.close()
+
+    cur = mydb.cursor()
+    cur.execute('SELECT * FROM productos')
+    datos2 = cur.fetchall()
+    cur.close()
+    
+    cur = mydb.cursor()
+    cur.execute('SELECT * FROM ventas')
+    datos3 = cur.fetchall()
+    cur.close()
+
+    return render_template('index.html', contactos=datos, articulos=datos2, ventas=datos3)
 
 @app.route('/add_empleados', methods=['POST'])
 def add_empleados():
     if request.method == 'POST':
+        idempleados = request.form['id']
         nombre = request.form['nombre']
         telefono = request.form['telefono']
         correo = request.form['correo']
-        cur = connection.cursor()
-        cur.execute('INSET INTO datos_empleados (nombre, telefono correo) VALUES (%s, %s ,%s)',
-                    (nombre, telefono, correo))
-        connection.commit()
+        cur = mydb.cursor()
+        cur.execute('INSERT INTO datos_empleados (idempleados, nombre, telefono, correo) VALUES (%s, %s, %s, %s)',
+                    (idempleados, nombre, telefono, correo))
+        mydb.commit()
+        return redirect(url_for('Index'))
+    
+@app.route('/add_productos', methods=['POST'])
+def add_productos():
+    if request.method == 'POST':
+        idproductos = request.form['idproducto']
+        articulo = request.form['articulo']
+        cantidad = request.form['cantidad']
+        precio = request.form['precio']
+        cur = mydb.cursor()
+        cur.execute('INSERT INTO productos (idproductos, articulo, cantidad, precio) VALUES (%s, %s, %s, %s)',
+                    (idproductos, articulo, cantidad, precio))
+        mydb.commit()
+        return redirect(url_for('Index'))
+    
+@app.route('/add_ventas', methods=['POST'])
+def add_ventas():
+    if request.method == 'POST':
+        idventas = request.form['idventas']
+        tipoventa = request.form['tipoventa']
+        idempleados = request.form['idempleados']
+        fecha = request.form['fecha']
+        vproductos = request.form['vproductos']
+        cur = mydb.cursor()
+        cur.execute('INSERT INTO ventas (idventas, tipo de venta, idempleados, fecha , productos) VALUES (%s ,%s, %s, %s, %s)',
+                    (idventas, tipoventa, idempleados, fecha, vproductos))
+        mydb.commit()
         return redirect(url_for('Index'))
